5370ee9ca4e98ff99746d3b9ff4d5ecfe4d0fb5d
phoi5675 <phoi5675@gmail.com>
2024-06-24 01:51:35 +0000

    Jodit의 ace editor 관련 img-src 정책 추가

--- src/constants/cspConfig.ts
+++ src/constants/cspConfig.ts
@@ -6,7 +6,7 @@ const defaultCspConfig: ContentSecurityPolicyType = Object.freeze({
     "'self'",
     "https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.4/", // NOTE: Jodit 페이지 진입하기 위해서는 해당 url 필요
   ]),
-  "img-src": new Set<string>(["'self'"]),
+  "img-src": new Set<string>(["'self'", "data:"]),
   "style-src": new Set<string>(["'self'"]),
   "frame-src": new Set<string>(["'self'"]),
 });
